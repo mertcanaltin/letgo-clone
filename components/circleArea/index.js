28cd0f7e433202552b82e55b48ce3b369042bbf8
mertcanaltin <mertcan.altin@akinon.com>
2022-04-18 10:26:53 +0300

    circleArea edited

--- components/circleArea/index.js
+++ components/circleArea/index.js
@@ -13,16 +13,29 @@ import {
   Pressable,
 } from "react-native";
 export default function circleArea() {
-
   return (
     <SafeAreaView>
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}  style={Style.circleArea}>
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={Style.circleArea}
+      >
         <View style={Style.container}>
-          <View style={[Style.square,Style.squareOne]} />
-          <View style={[Style.square,Style.squareTwo]} />
-          <View style={[Style.square,Style.squareTree]} />
-          <View style={[Style.square,Style.squareFour]} />
-          <View style={[Style.square,Style.squareFive]} />
+          <View style={[Style.square, Style.squareOne]}>
+            <Text style={Style.squareText}>Araba</Text>
+          </View>
+          <View style={[Style.square, Style.squareTwo]}>
+            <Text style={Style.squareText}>Elektronik</Text>
+          </View>
+          <View style={[Style.square, Style.squareTree]}>
+            <Text style={Style.squareText}>Telefon</Text>
+          </View>
+          <View style={[Style.square, Style.squareFour]}>
+            <Text style={Style.squareText}>Tablet</Text>
+          </View>
+          <View style={[Style.square, Style.squareFive]}>
+            <Text style={Style.squareText}>M1</Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
